bc963b372c87208c77d9080fcaa3031d597cb804
NabrAj MadAi <nabraj0444@gmail.com>
2024-02-23 07:08:58 +0545

    python info and operators

---
+++ input_variables.py
@@ -0,0 +1,10 @@
+#input_variable.py
+a = int(input("Enter a value of a: "))
+b = int(input("Enter a value of b: "))
+print("Multiply",a*b)
+print("Addition",a+b)
+print("subtraction",a-b)
+print("division",a/b)
+print("modules",a%b)
+
+# learn f-string and do above program   
---
+++ operators.py
@@ -0,0 +1,57 @@
+# Addition 
+# operator +
+n = 444 + 777 
+print(n)
+m = 44+11.5 
+print(m)
+a = "NAbraj "+"Madai"
+print(a)
+b = "navya"+ "1"
+print(b)
+'''
+c = "navya"+1 
+print(c) 
+'''
+# subtraction
+# operator -
+a = 44-31
+print(a)
+b = 23-32-324
+print(b)
+
+# multiplication
+# operators *
+a = 4*4
+print(a)
+word_multiplication = "Navya "*11
+print(word_multiplication)
+
+# Division
+# operators /
+a = 4444/11
+print(a)
+'''   
+b = "Nabraj"/44
+print(b)
+c = "String"/"string"
+print(c)
+Traceback (most recent call last):
+  File "E:\Broadway Python and Django\operators.py", line 26, in <module>
+    b = "Nabraj"/44
+        ~~~~~~~~^~~
+TypeError: unsupported operand type(s) for /: 'str' and 'int'
+'''
+d = 20/4
+print(d)
+
+#Modules
+a = 11%2 #1 reminder
+print(a)
+b = 10%5 #0 reminder
+print(b)
+
+#Exponentiation
+a = 2**3
+A = 10**10
+print(a)
+print(A)
